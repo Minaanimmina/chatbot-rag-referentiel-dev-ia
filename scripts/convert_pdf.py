0647f7cd1926c0c348041adfe8002a1282e06a0d
Minaanimmina <cracmina69@hotmail.fr>
2026-04-27 16:55:57 +0200

    feat(convert-pdf): add function to clean markdown file after conversion

--- scripts/convert_pdf.py
+++ scripts/convert_pdf.py
@@ -6,8 +6,23 @@ from the marker library.
 from config import PDF_PATH, DATA_PATH
 import pymupdf4llm
 import json
+import re
 
 md_text = pymupdf4llm.to_markdown(PDF_PATH)
+
+
+def clean_markdown(text):
+    # Supprimer les <br>
+    text = text.replace("<br>", "\n")
+    # Supprimer les numéros de page
+    text = re.sub(r"Page \*\*\d+\*\* / \*\*25\*\*", "", text)
+    # Supprimer les références images
+    picture_pattern = "**==> picture [91 x 32] intentionally omitted <==**"
+    text = text.replace(picture_pattern, "")
+    return text
+
+
+md_text = clean_markdown(md_text)
 
 with open(DATA_PATH, "w", encoding="utf-8") as f:
     if isinstance(md_text, str):
